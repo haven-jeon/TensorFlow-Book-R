c3540c0ad37645d8e8412c4a5c50c761542eaaf7
haven-jeon <madjakarta@gmail.com>
2017-06-01 13:47:29 +0900

    RefClass to R6

--- ch07_autoencoder/Concept01_autoencoder.R
+++ ch07_autoencoder/Concept01_autoencoder.R
@@ -10,68 +10,70 @@ get_batch <- function(X, size){
 
 ## ------------------------------------------------------------------------
 
-
-Autoencoder <- setRefClass("Autoencoder",
-  fields = c('input_dim', 'hidden_dim', 'epoch', 'batch_size', 
-             'learning_rate', 'x', 'encoded', 'decoded', 'loss', 'all_loss',
-             'train_op','saver', 'autoencoder_graph'),
-  methods=list(
+library(R6)
+Autoencoder <- R6Class("Autoencoder",
+  public = list(input_dim=NULL, hidden_dim=NULL, epoch=NULL, batch_size=NULL, 
+             learning_rate=NULL, x=NULL, encoded=NULL, decoded=NULL, loss=NULL, all_loss=NULL,
+             train_op=NULL,saver=NULL, autoencoder_graph=NULL,
+             
     initialize=function(input_dim, hidden_dim, epoch=500, batch_size=10, learning_rate=0.001){
-      .self$epoch <- as.integer(epoch)
-      .self$batch_size <- as.integer(batch_size)
-      .self$learning_rate <- learning_rate
-      .self$input_dim <- as.integer(input_dim)
-      .self$hidden_dim <- as.integer(hidden_dim)
+      self$epoch <- as.integer(epoch)
+      self$batch_size <- as.integer(batch_size)
+      self$learning_rate <- learning_rate
+      self$input_dim <- as.integer(input_dim)
+      self$hidden_dim <- as.integer(hidden_dim)
       
       # make graph for avoiding restore error 
-      .self$autoencoder_graph <- tf$Graph()
+      self$autoencoder_graph <- tf$Graph()
       
-      with(.self$autoencoder_graph$as_default(), {
-        .self$x <- tf$placeholder(dtype=tf$float32, shape=list(NULL, .self$input_dim))
+      with(self$autoencoder_graph$as_default(), {
+        self$x <- tf$placeholder(dtype=tf$float32, shape=list(NULL, self$input_dim))
         with(tf$name_scope('encode'), {
-              encode_weights <- tf$Variable(tf$random_normal(list(.self$input_dim, .self$hidden_dim),
+              encode_weights <- tf$Variable(tf$random_normal(list(self$input_dim, self$hidden_dim),
                                                       dtype=tf$float32), name='weights')
-              encode_biases <- tf$Variable(tf$zeros(list(.self$hidden_dim)), name='biases')
-              .self$encoded  <- tf$nn$sigmoid(tf$matmul(x, encode_weights) + encode_biases)
+              encode_biases <- tf$Variable(tf$zeros(list(self$hidden_dim)), name='biases')
+              self$encoded  <- tf$nn$sigmoid(tf$matmul(self$x, encode_weights) + encode_biases)
         })
         
         with(tf$name_scope('decode'),{
-              decode_weights <- tf$Variable(tf$random_normal(list(.self$hidden_dim, .self$input_dim),
+              decode_weights <- tf$Variable(tf$random_normal(list(self$hidden_dim, self$input_dim),
                                                       dtype=tf$float32), name='weights')
-              decode_biases <- tf$Variable(tf$zeros(list(.self$input_dim)), name='biases')
-              .self$decoded <- tf$matmul(encoded, decode_weights) + decode_biases
+              decode_biases <- tf$Variable(tf$zeros(list(self$input_dim)), name='biases')
+              self$decoded <- tf$matmul(self$encoded, decode_weights) + decode_biases
         })
   
         # Define cost function and training op
-        .self$loss <- tf$sqrt(tf$reduce_mean(tf$square(tf$subtract(.self$x, .self$decoded))))
+        self$loss <- tf$sqrt(tf$reduce_mean(tf$square(tf$subtract(self$x, self$decoded))))
   
-        .self$all_loss <- tf$sqrt(tf$reduce_mean(tf$square(tf$subtract(.self$x, .self$decoded)), 1L))
-        .self$train_op <- tf$train$AdamOptimizer(.self$learning_rate)$minimize(.self$loss)
+        self$all_loss <- tf$sqrt(tf$reduce_mean(tf$square(tf$subtract(self$x, self$decoded)), 1L))
+        self$train_op <- tf$train$AdamOptimizer(self$learning_rate)$minimize(self$loss)
           
         # Define a saver op
-        .self$saver = tf$train$Saver()
+        self$saver <- tf$train$Saver()
       })
       },
     train=function(data){
-      with(tf$Session(graph=.self$autoencoder_graph) %as% sess,{
+      with(tf$Session(graph=self$autoencoder_graph) %as% sess,{
         sess$run(tf$global_variables_initializer())
-        for(i in 1:.self$epoch){
+        for(i in 1:self$epoch){
           for(j in 1:500){
-            batch_data <- get_batch(data, .self$batch_size)
-            l_ <- sess$run(list(.self$loss, .self$train_op), feed_dict=dict(x=batch_data))
+            batch_data <- get_batch(data, self$batch_size)
+            x <- self$x
+            l_ <- sess$run(list(self$loss, self$train_op), feed_dict=dict(x=batch_data))
           }
           if(i %% 50 == 0){
             print(sprintf('epoch %d: loss = %f', i, l_[[1]]))
-            .self$saver$save(sess, './model.ckpt')
+            self$saver$save(sess, './model.ckpt')
           }
         }
-        .self$saver$save(sess, './model.ckpt')
+        self$saver$save(sess, './model.ckpt')
         })
       },
     test=function(data){
-      with(tf$Session(graph=.self$autoencoder_graph) %as% sess, {
-        .self$saver$restore(sess, './model.ckpt')
-        hidden_reconstructed <- sess$run(list(.self$encoded, .self$decoded), feed_dict=dict(x=data))
+      with(tf$Session(graph=self$autoencoder_graph) %as% sess, {
+        self$saver$restore(sess, './model.ckpt')
+        x <- self$x
+        hidden_reconstructed <- sess$run(list(self$encoded, self$decoded), feed_dict=dict(x=data))
         })
       print(paste('input', data))
       print(paste('compressed', hidden_reconstructed[[1]]))
@@ -79,20 +81,21 @@ Autoencoder <- setRefClass("Autoencoder",
       return(hidden_reconstructed[[2]])
       },
     get_params=function(){
-      with(tf$Session(graph=.self$autoencoder_graph) %as% sess, {
-        .self$saver$restore(sess, './model.ckpt')
-        weight_biases <- sess$run(list(.self$weight1, .self$biases1))
+      with(tf$Session(graph=self$autoencoder_graph) %as% sess, {
+        self$saver$restore(sess, './model.ckpt')
+        weight_biases <- sess$run(list(self$weight1, self$biases1))
       })
       return(list(weight=weight_biases[[1]], biases=weight_biases[[2]]))
       },
     classify=function(data, labels){
-      with(tf$Session(graph=.self$autoencoder_graph) %as% sess, {
+      with(tf$Session(graph=self$autoencoder_graph) %as% sess, {
         sess$run(tf$global_variables_initializer())
-        .self$saver$restore(sess, './model.ckpt')
-        hidden_reconstructed <- sess$run(list(.self$encoded, .self$decoded), feed_dict=dict(x=data))
+        self$saver$restore(sess, './model.ckpt')
+        x <- self$x
+        hidden_reconstructed <- sess$run(list(self$encoded, self$decoded), feed_dict=dict(x=data))
         reconstructed <- hidden_reconstructed[[2]]
         print(dim(reconstructed))
-        # loss <- sess$run(.self$all_loss, feed_dict=dict(x: data))
+        # loss <- sess$run(self$all_loss, feed_dict=dict(x: data))
         print(paste('data', dim(data)))
         print(paste('reconstructed', dim(hidden_reconstructed[[2]])))
         loss_test <- apply((data - hidden_reconstructed[[2]])^2, 1, function(x){sqrt(mean(x))})
@@ -107,10 +110,11 @@ Autoencoder <- setRefClass("Autoencoder",
       return(hidden_reconstructed[[1]])
       },
     decode=function(encoding){
-      with(tf$Session(graph=.self$autoencoder_graph) %as% sess, {
+      with(tf$Session(graph=self$autoencoder_graph) %as% sess, {
             sess$run(tf$global_variables_initializer())
-            .self$saver$restore(sess, './model.ckpt')
-            reconstructed <- sess$run(.self$decoded, feed_dict=dict(encoded= encoding))
+            self$saver$restore(sess, './model.ckpt')
+            encoded <- self$encoded
+            reconstructed <- sess$run(self$decoded, feed_dict=dict(encoded= encoding))
       })
       img <- matrix(reconstructed,byrow = T, nrow = 32)
       return(img)
--- ch07_autoencoder/Concept02_autoencoder_with_imgs.md
+++ ch07_autoencoder/Concept02_autoencoder_with_imgs.md
@@ -88,16 +88,16 @@ ae <- Autoencoder$new(input_dim, hidden_dim)
 ae$train(horse_x)
 ```
 
-    ## [1] "epoch 50: loss = 46.522511"
-    ## [1] "epoch 100: loss = 47.680244"
-    ## [1] "epoch 150: loss = 48.945663"
-    ## [1] "epoch 200: loss = 58.313343"
-    ## [1] "epoch 250: loss = 50.077698"
-    ## [1] "epoch 300: loss = 54.402054"
-    ## [1] "epoch 350: loss = 51.897457"
-    ## [1] "epoch 400: loss = 47.022156"
-    ## [1] "epoch 450: loss = 51.340378"
-    ## [1] "epoch 500: loss = 57.876442"
+    ## [1] "epoch 50: loss = 46.674625"
+    ## [1] "epoch 100: loss = 46.550663"
+    ## [1] "epoch 150: loss = 50.245029"
+    ## [1] "epoch 200: loss = 57.250927"
+    ## [1] "epoch 250: loss = 50.494396"
+    ## [1] "epoch 300: loss = 53.319695"
+    ## [1] "epoch 350: loss = 51.553219"
+    ## [1] "epoch 400: loss = 45.833340"
+    ## [1] "epoch 450: loss = 51.136341"
+    ## [1] "epoch 500: loss = 55.668030"
 
     ## [1] "./model.ckpt"
 
@@ -112,8 +112,8 @@ encodings <- ae$classify(test_x, test_labels)
     ## [1] "data 50000" "data 1024" 
     ## [1] "reconstructed 50000" "reconstructed 1024" 
     ## [1] "loss length 50000"
-    ## [1] "horse 51.3009221892531"
-    ## [1] "not horse 50.9304309871452"
+    ## [1] "horse 51.5855043091544"
+    ## [1] "not horse 51.1346412026076"
 
 ``` r
 par(mfrow=c(10,2), mar = rep(1, 4))
